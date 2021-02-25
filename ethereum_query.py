05714432836725b38da2c72a95046b7fe09f593d
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-25 17:07:06 -0500

    x

--- ethereum_query.py
+++ ethereum_query.py
@@ -17,12 +17,12 @@ def getTransaction(tx):
 
 def getGasPrice(tx):
     block = w3.eth.getTransaction(tx) 
-    gasPrice = block.gasPrice
+    gasPrice = block.gas
     print(gasPrice)
     return gasPrice
 
 def getGas(tx):
-    block = w3.eth.getTransaction(tx) 
+    block = w3.eth.getTransactionByBlock(tx) 
     gas = block.gas
     print(gas)
     return gas
@@ -40,3 +40,16 @@ def getMostExpensiveTransaction(blockNum):
     maxTx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
     return maxTx
 
+
+tx = '0xa1cb58ac3d7457994feecb5680088d2aa26dc0ac1af54560a2d131925713a820'  
+
+tx2 = '0xe58711fc27daecbc3b099c6d5ebb0e92646e784fc30d653f006ee93ba30ffd52' 
+#used gas 21000 but your function returned 166666
+
+tx3 = '0x8926ced59631388cbb356ecf67d23e5d89a36f93eade2e4163e983545c3fc5b9' 
+#used gas 21000 but your function returned 23100
+
+block = w3.eth.getTransaction(tx) 
+print(block.blockNumber)
+# getGasPrice(tx)
+print(block.gas)
