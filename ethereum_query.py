55ca4985dd982ac40817aa11f97224bbf5c0580b
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-25 17:08:43 -0500

    x

--- ethereum_query.py
+++ ethereum_query.py
@@ -18,13 +18,11 @@ def getTransaction(tx):
 def getGasPrice(tx):
     block = w3.eth.getTransaction(tx) 
     gasPrice = block.gas
-    print(gasPrice)
     return gasPrice
 
 def getGas(tx):
-    block = w3.eth.getTransactionByBlock(tx) 
+    block = w3.eth.getTransaction(tx) 
     gas = block.gas
-    print(gas)
     return gas
 
 def getTransactionCost(tx):
@@ -41,15 +39,15 @@ def getMostExpensiveTransaction(blockNum):
     return maxTx
 
 
-tx = '0xa1cb58ac3d7457994feecb5680088d2aa26dc0ac1af54560a2d131925713a820'  
+# tx = '0xa1cb58ac3d7457994feecb5680088d2aa26dc0ac1af54560a2d131925713a820'  
 
-tx2 = '0xe58711fc27daecbc3b099c6d5ebb0e92646e784fc30d653f006ee93ba30ffd52' 
+# tx2 = '0xe58711fc27daecbc3b099c6d5ebb0e92646e784fc30d653f006ee93ba30ffd52' 
 #used gas 21000 but your function returned 166666
 
-tx3 = '0x8926ced59631388cbb356ecf67d23e5d89a36f93eade2e4163e983545c3fc5b9' 
+# tx3 = '0x8926ced59631388cbb356ecf67d23e5d89a36f93eade2e4163e983545c3fc5b9' 
 #used gas 21000 but your function returned 23100
 
-block = w3.eth.getTransaction(tx) 
-print(block.blockNumber)
+# block = w3.eth.getTransaction(tx) 
+# print(block.blockNumber)
 # getGasPrice(tx)
-print(block.gas)
+# print(block.gas)
