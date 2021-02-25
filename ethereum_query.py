5d1f73aa250daf833cc593db667d30de96a935d6
aorlander <49505752+aorlander@users.noreply.github.com>
2021-02-25 17:18:40 -0500

    x

--- ethereum_query.py
+++ ethereum_query.py
@@ -18,7 +18,7 @@ def getTransaction(tx):
 
 def getGasPrice(tx):
     block = w3.eth.getTransaction(tx) 
-    gasPrice = block.gas
+    gasPrice = block.gasPrice
     return gasPrice
 
   # Next, complete the function getGas that takes a transaction and returns the amount of gas used by the transaction.
